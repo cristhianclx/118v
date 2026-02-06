3e1d0faf23b02de89c033c7d1855d9db2c802501
Cristhian <cristhianclx@gmail.com>
2026-02-05 22:29:44 -0500

    06

--- 06/main.py
+++ 06/main.py
@@ -26,6 +26,19 @@ class User(db.Model):
         return "<User: {}>".format(self.id)
 
 
+class Message(db.Model):
+    __tablename__ = "messages"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=True)
+    importance = db.Column(db.String(100), nullable=True)
+    created = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    user = db.relationship("User", backref="user")
+    def __repr__(self):
+        return "<Message: {}>".format(self.id)
+
+
 class HealthResource(Resource):
     def get(self):
         return {
@@ -67,40 +80,85 @@ class UsersResource(Resource):
         }, 201
 
 
+class UsersIDResource(Resource):
+    def get(self, id):
+        item = User.query.get_or_404(id)
+        return {
+            "id": item.id,
+            "code": item.code,
+            "first_name": item.first_name,
+            "last_name": item.last_name,
+            "age": item.age,
+        }
+    
+    def patch(self, id):
+        item = User.query.get_or_404(id)
+        data = request.get_json()
+        item.code = data.get("code", item.code)
+        item.first_name = data.get("first_name", item.first_name)
+        item.last_name = data.get("last_name", item.last_name)
+        item.age = data.get("age", item.age)
+        db.session.add(item)
+        db.session.commit()
+        return {
+            "id": item.id,
+            "code": item.code,
+            "first_name": item.first_name,
+            "last_name": item.last_name,
+            "age": item.age,
+        }
+
+    def delete(self, id):
+        item = User.query.get_or_404(id)
+        db.session.delete(item)
+        db.session.commit()
+        return {}, 204
+
+
+class MessagesResource(Resource):
+    def get(self):
+        items = Message.query.all()
+        data = []
+        for x in items:
+            data.append({
+                "id": x.id,
+                "title": x.title,
+                "content": x.content,
+                "importance": x.importance,
+                "user_id": x.user_id,
+            })
+        return data
+
+    def post(self):
+        data = request.get_json()
+        item = Message(
+            title=data["title"],
+            content=data["content"],
+            importance=data["importance"],
+            user_id=data["user_id"],
+        )
+        db.session.add(item)
+        db.session.commit()
+        return {
+            "id": item.id,
+            "title": item.title,
+            "content": item.content,
+            "importance": item.importance,
+            "user_id": item.user_id,
+        }, 201
+
+
 api.add_resource(HealthResource, "/health")
 api.add_resource(UsersResource, "/users")
+api.add_resource(UsersIDResource, "/users/<int:id>")
+api.add_resource(MessagesResource, "/messages")
 
 
-##
-## crear la tabla message (04) (id, title*, content, importance*, user_id, user, created)
-## correr la migracion
-##    flask --app main db migrate # crea la migracion
-##    flask --app main db upgrade # aplicar la migracion
-## REST = /messages/
-##          GET =>
-##          POST =>
-##
-
-
-
-
-
-
-
-
-
-"""
-  /users/<id>
-    GET:
-      parametros: -
-      response: { id: 1, first: aaa, last: bbb}
-      status response: 200
-    PATCH/PUT:
-      parametros: { last: ccc}
-      response: { id: 1, first: aaa, last: ccc }
-      status response: 200
-    DELETE:
-      parametros: -
-      response: -
-      status response: 204 # algo paso pero no tengo nada que devolver
- """
+## LABORATORIO
+## messages (priority => nuevo campo)
+##    flask --app main db migrate
+#     flask --app main db upgrade
+## /messages/<id> REST
+##   GET => { id: 1, .... }
+##   PATCH ({title: 1234}) => { id: 1, ... }
+##   DELETE => {}
